147e2b17cd4da7319c915679889ed236debd7047
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-08-27 19:42:52 -0400

    styled the navbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
 import './home.css'
 
 class Home extends Component {
@@ -12,13 +13,13 @@ class Home extends Component {
     render() {
         return (
             <>
+
             <Container fluid="md">
                 <Row>
                     <Col>
                         <div>
                             <img src="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2Fhomepagelogo.png?v=1598568102862" />
                         </div>
-
                     </Col>
                 </Row>
             </Container>
