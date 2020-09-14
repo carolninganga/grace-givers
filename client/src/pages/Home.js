ecfe62d5836b6b584c91b23f1a26b003efcc54ef
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-09-13 22:58:06 -0400

    added responsiveness

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,8 @@ import Col from 'react-bootstrap/Col';
 
 import './home.css'
 
+
+
 class Home extends Component {
    
 
@@ -18,15 +20,43 @@ class Home extends Component {
                 <Row>
                     <Col>
                         <div>
-                            <img src="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2Fhomepagelogo.png?v=1598568102862" />
+                            <img sytle={{width:"300"}} src="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2Fhomepagelogo.png?v=1598568102862" />
                         </div>
                         <div className="discription">
                             <h2> Our mission</h2>
                             <p className="mission">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                            Why do we use it?
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                            Contribute to national development through constructive and supportive
+                            engagement with communities to enable children enjoy life and develop social
+                            skills.
                             </p>
+                            <h2> Our Vision</h2>
+                            <p className="mission">
+                            Mobilise communities in advancement of child and women rights and human
+                            development by using education, health and environmental programs.
+                            </p>
+                            <h2> Goals and Objectives</h2>
+                            <p className="mission">
+                            The goal of Grace Givers is to help children enjoy life and develop their
+                            intellectual capacity as they grow to maturity. To organisation will achieve this
+                            through the following specific objectives. In addition, we want to empower
+                            women in daily activities such as food preparation, health tips and also
+                            productive but low cost farming methods.
+                            </p>
+                            <ul>
+                                <li className="listItem">
+                                To provide support to children through community based groups such
+                                as childcare (feed the children trust) centers, educational centers and
+                                other community structures (kitchen, library, and game center).
+                                </li>
+                                <li className="listItem">
+                                To introduce innovative approaches for empowering children, families
+                                and communities (training programs).
+                                </li>
+                                <li className="listItem">
+                                To impart knowledge and skills by working with groups of children and
+                                women in their communities
+                                </li>
+                            </ul>
                             {/* <div className="sociallinks">
                             <a href="carolninganga@gmail.com"><i className="fab fa-instagram-square"></i></a>
                             <a target="_blank"  href="https://github.com/carolninganga"><i className="fab fa-facebook-square"></i></a>
